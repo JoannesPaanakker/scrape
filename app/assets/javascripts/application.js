e68ae88b69dc720947eff0ea3278f686d7e4d0c0
leon henry <leonivanhenry@gmail.com>
2018-03-07 10:49:28 +0100

    updated prices

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,8 @@
 //= require jquery
-//= require modernizr
-//= require pace.min
-//= require plugins
 //= require cable
-//= require gentelella-custom
 //= require bootstrap.min
-//= require dashboard/flot-data
-//= require dashboard/metisMenu.min
-//= require dashboard/morris-data
-//= require dashboard/morris.min
-//= require dashboard/raphael.min
 //= require dashboard/startmin
 //= require form_index
+//= require plugins
 
 
